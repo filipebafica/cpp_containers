c8e19623595b0467d587af23d30418f814c0bfd0
Filipe Báfica <fbafica@DESKTOP-PMPGAI5.localdomain>
2022-09-28 14:32:54 -0300

    test: vector var number of elements perfomance test

--- tests/ft_vector_perfomance_test.cpp
+++ tests/ft_vector_perfomance_test.cpp
@@ -6,39 +6,41 @@
 
 int main(void) {
     clock_t t;
+    int     num_elements;
 
     t = std::clock();
+    num_elements = 100000;
     {
         std::cout << "[TESTING INSERTION (BEGIN)]" << std::endl;
-        ft::vector<int> ft_v(100000, 1);
-        for (int i = 0; i < 100000; i++)
+        ft::vector<int> ft_v(num_elements, 1);
+        for (int i = 0; i < num_elements; i++)
             ft_v.insert(ft_v.begin(), i);
         std::cout << "STATUS: OK" << std::endl;
     }
     {
         std::cout << "[TESTING INSERTION (MIDLLE)]" << std::endl;
-        ft::vector<int> ft_v(100000, 1);
-        for (int i = 0; i < 100000; i++)
-            ft_v.insert(ft_v.begin() + 50000, i);
+        ft::vector<int> ft_v(num_elements, 1);
+        for (int i = 0; i < num_elements; i++)
+            ft_v.insert(ft_v.begin() + (num_elements / 2), i);
         std::cout << "STATUS: OK" << std::endl;
     }
     {
         std::cout << "[TESTING INSERTION (END)]" << std::endl;
-        ft::vector<int> ft_v(100000, 1);
-        for (int i = 0; i < 100000; i++)
+        ft::vector<int> ft_v(num_elements, 1);
+        for (int i = 0; i < num_elements; i++)
             ft_v.insert(ft_v.end(), i);
         std::cout << "STATUS: OK" << std::endl;
     }
     {
         std::cout << "[TESTING SWAP]" << std::endl;
-        ft::vector<int> ft_v_a(100000, 1);
-        ft::vector<int> ft_v_b(100000, 0);
+        ft::vector<int> ft_v_a(num_elements, 1);
+        ft::vector<int> ft_v_b(num_elements, 0);
         ft_v_a.swap(ft_v_b);
         std::cout << "STATUS: OK" << std::endl;
     }
     {
         std::cout << "[TESTING ERASE]" << std::endl;
-        ft::vector<int> ft_v(100000, 1);
+        ft::vector<int> ft_v(num_elements, 1);
         ft_v.erase(ft_v.begin(), ft_v.end());
         std::cout << "STATUS: OK" << std::endl;
     }
